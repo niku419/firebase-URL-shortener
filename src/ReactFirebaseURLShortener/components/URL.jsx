0608f12563d83d4c6eedcba6d2d634f1c5b73fe4
Niku_419 <2019198@iiitdmj.ac.in>
2021-07-06 13:19:44 +0530

    :rocket: deploying to firebase with some styles

--- src/ReactFirebaseURLShortener/components/URL.jsx
+++ src/ReactFirebaseURLShortener/components/URL.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import {Container, Form, Button, Jumbotron, Alert, Spinner, Nav, Navbar} from 'react-bootstrap'
+import {Container, Form, Button, Alert, Spinner, Nav, Navbar} from 'react-bootstrap'
 // import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import { faInstagram, faGithub, faFacebookF, faLinkedinIn} from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -36,19 +36,19 @@ export default function URL() {
     )
   }
   return (
-    <Container className="pt-4">
+    <Container className="pt-5 main">
       <Container>
-        <Jumbotron>
-          <h1 style={{textAlign: "center"}}>URL Shortener</h1>
-          <div className="mt-5">
-            <em>
+        <div className="pb-5 bord">
+          <h1 className="heading"><span className="url">URL</span> Shortener</h1>
+          <div className="mt-4 text-center">
+            <em className="description">
               This URL Shortener is a tool that creates a short, unique URL that will redirect to the specific website of your choosing using Firebase & React
             </em>
           </div>
-        </Jumbotron>
-        <Form>
+        </div>
+        <Form className="mt-4">
           <Form.Group>
-            <div className="d-flex">
+            <div className="d-flex" >
               <div className="flex-grow-1">
                 <Form.Control 
                   type="text" 
@@ -61,7 +61,7 @@ export default function URL() {
                 />
               </div>
               <div>
-                <Button type="submit" variant="outline-secondary" onClick={handleClick}>
+                <Button type="submit" className="ml-2 button" onClick={handleClick}>
                   Convert
                 </Button>
               </div>
@@ -70,7 +70,7 @@ export default function URL() {
         </Form>
       </Container>
       {click && <Container>
-        <Alert variant="secondary" className="d-flex justify-content-between">
+        <Alert variant="warning" className="d-flex justify-content-between">
           <Alert.Link href={shorturl}>
             <div className="pt-1">
               {shorturl}
@@ -78,33 +78,33 @@ export default function URL() {
           </Alert.Link>
           <CopyToClipboard text={shorturl} onCopy={()=> setClipboardClick(true)}>
             {clipboardClick ?
-            <Button variant="outline-secondary">
-              <FontAwesomeIcon icon={faClipboardCheck} />
+            <Button variant="light">
+              <FontAwesomeIcon icon={faClipboardCheck} color="#e85a4f"/>
             </Button> :
-            <Button variant="outline-secondary">
-                <FontAwesomeIcon icon={faClipboard} />
+            <Button variant="light">
+                <FontAwesomeIcon color="#e85a4f" icon={faClipboard} />
             </Button>}
         </CopyToClipboard>
         </Alert>
       </Container>}
       <Container>
-			<Navbar bg="transparent" fixed="bottom" variant="light">
-				<Navbar.Brand >
-					<strong>Niku419</strong>
-				</Navbar.Brand>
-				<Nav className="mr-auto">
-					<Nav.Link href="https://github.com/niku419"><FontAwesomeIcon  icon={faGithub} /></Nav.Link>
-					<Nav.Link href="https://linkedin.com/niku_419"><FontAwesomeIcon  icon={faLinkedinIn} /></Nav.Link>
-					<Nav.Link href="https://instagram.com/_niku_419"><FontAwesomeIcon  icon={faInstagram} /></Nav.Link>
-					<Nav.Link href="#"><FontAwesomeIcon icon={faFacebookF} /></Nav.Link>
-				</Nav>
-				<Form inline>
-					{/* <Nav className="mr-auto"> 
-						<Nav.Link>made for Hiku<FontAwesomeIcon color="#8d0101" icon={faHeart} /></Nav.Link>
-					</Nav> */}
-				</Form>
-		</Navbar>
-		</Container>
-   </Container> 
+        <Navbar bg="transparent" fixed="bottom" variant="light">
+          <Navbar.Brand >
+            <strong style={{color: "#e8584f"}}>Niku419</strong>
+          </Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="https://github.com/niku419"><FontAwesomeIcon color="#e8584f" icon={faGithub} /></Nav.Link>
+            <Nav.Link href="https://linkedin.com/niku_419"><FontAwesomeIcon color="#e8584f"  icon={faLinkedinIn} /></Nav.Link>
+            <Nav.Link href="https://instagram.com/_niku_419"><FontAwesomeIcon color="#e8584f"  icon={faInstagram} /></Nav.Link>
+            <Nav.Link href="https://www.facebook.com/profile.php?id=100069976086066"><FontAwesomeIcon color="#e8584f" icon={faFacebookF} /></Nav.Link>
+          </Nav>
+          <Form inline>
+            {/* <Nav className="mr-auto"> 
+              <Nav.Link>made for Hiku<FontAwesomeIcon color="#8d0101" icon={faHeart} /></Nav.Link>
+            </Nav> */}
+          </Form>
+        </Navbar>
+		  </Container>
+    </Container> 
   )
 } 
